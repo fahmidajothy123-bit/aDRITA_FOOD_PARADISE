b7e4d43dfb272107cf860e507bb42ba0bff8d58f
Adrita32 <c221232@ugrad.iiuc.ac.bd>
2026-07-11 21:19:59 +0600

    Delete backend directory

--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createContactMessage, getContactMessages } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", createContactMessage);
-router.get("/", getContactMessages);
-
-export default router;
